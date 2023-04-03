177b930576b6d8aafd4b03df4344f58257ff508d
lucas <lucas.pr1@hotmail.com>
2023-04-03 16:47:35 -0300

    Fixed spaces

--- Day 1/Band name.py
+++ Day 1/Band name.py
@@ -5,6 +5,6 @@ city = input("What is the name of the city you grew up in?\n")
 # 3. Ask the user for the name of a pet.
 pet = input("what is the name of your pet?\n")
 # 4. Combine the name of their city and pet and show them their band name.
-print("your band name could be"+city+" "+pet+"\n")
+print("your band name could be"+" "+city+" "+pet+"\n")
 
 
