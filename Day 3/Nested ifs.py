1a2b8bce89dafe2d8abc4eccec9473a491e2775c
lucas <lucas.pr1@hotmail.com>
2023-04-05 08:03:41 -0300

    Added photo question

--- Day 3/Nested ifs.py
+++ Day 3/Nested ifs.py
@@ -1,14 +1,26 @@
-#nested conditionals
+# nested conditionals
 
-height = int(input("what is your height?"))
+height = int(input("what is your height in centimeters?"))
+bill = 0
 if height >= 120:
     print("you're tall enough for the ride")
+
     age = int(input("what is your age?"))
     if age < 12:
-        print("Please pay 5$")
+        bill = bill + 5
+        print("Your ticket will be 5$")
     elif age <= 18:
-        print("please pay 7$")
+        bill = bill + 7
+        print("Your ticket will be 7$")
     elif age > 18:
-        print("please pay 12$")
+        bill = bill + 12
+        print("Your ticket will be 12$")
+
+    wants_photo = str(input("Do you want photos of the ride? price: 3$ (Y/N)"))
+
+    if wants_photo == "Y" or wants_photo == "y":
+        bill += 3
+
+    print(f"Total price:{bill}")
 else:
     print("you cant ride the rollercoaster")
