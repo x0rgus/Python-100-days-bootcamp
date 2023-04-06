9a76406e67d94916fcdaa749dd6381d47b3090ea
lucas <lucas.pr1@hotmail.com>
2023-04-06 12:56:27 -0300

    random index

--- Day 4/Random module.py
+++ Day 4/Random module.py
@@ -1,7 +1,11 @@
+# Random number and index demo
 import random
 
-result = random.randint(1,2)
-if result == 1:
-    print("Heads")
-else:
-    print("Tails")
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+npeople = (len(names))
+random_choice = random.randint(0, npeople - 1)
+payer = names[random_choice]
+print(f"{payer} is going to buy the meal today!")
