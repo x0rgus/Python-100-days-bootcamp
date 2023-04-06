312e80ec02fc58d0ff0f8bb2e8d5a96d8516efef
lucas <lucas.pr1@hotmail.com>
2023-04-06 14:48:01 -0300

    added treasure marker

---
+++ Day 4/treasure marker.py
@@ -0,0 +1,14 @@
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+position = list(position)
+X = int(position[0])
+Y = int(position[1])
+selection = map[X - 1]
+selection[Y - 1] = "'x'"
+
+print(f"{row1}\n{row2}\n{row3}")
