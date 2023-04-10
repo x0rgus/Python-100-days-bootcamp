07f8f10296ccd7dbd5f18653df917b3723c94f7e
lucas <lucas.pr1@hotmail.com>
2023-04-10 12:06:29 -0300

    Randomized order of characters

--- Day 5/Password generator project.py
+++ Day 5/Password generator project.py
@@ -8,14 +8,15 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+passwordchars = []
 password = ""
-
 for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+    passwordchars.append(random.choice(letters))
 for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+    passwordchars.append(random.choice(symbols))
 for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
-
+    passwordchars.append(random.choice(numbers))
+for char in range(1, len(passwordchars)+1):
+    password += random.choice(passwordchars)
 
 print(password)
