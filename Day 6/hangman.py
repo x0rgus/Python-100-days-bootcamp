1e45d225668a0f7698c034c9c92f3c0e2d07e176
lucas <lucas.pr1@hotmail.com>
2023-04-19 21:05:57 -0300

    minor changes

--- Day 6/hangman.py
+++ Day 6/hangman.py
@@ -8,7 +8,17 @@ word = words[word_index]
 word = [x for x in word]
 MAX_WRONG = len(HANGMANPICS) -1
 blankletters = len(word)
+display = []
 
 print(HANGMANPICS[0], end="")
-for space in word:
-    print("_ ", end="")
+for letter in word:
+    display.append("_")
+print(display)
+userinput = input("\n Please type a letter")
+userinput = userinput.lower()
+
+for letter in word:
+    if userinput == letter:
+        print("correct letter")
+    elif not userinput == letter:
+        print("incorrect letter")
