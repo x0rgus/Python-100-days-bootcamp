6294be31886d62edfb94fbb9a6fa1985774d7cad
x0rgus <lucas.pr1@hotmail.com>
2023-05-10 12:16:27 -0300

    simplified code

--- Day 7/Ceaser cipher.py
+++ Day 7/Ceaser cipher.py
@@ -5,26 +5,26 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-def decrypt(encrypted_text, shift_amount):
-    plain_text = ""
-    for letter in encrypted_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
 
 
-match direction:
-    case "encode":
-        encrypt(text, shift)
-    case "decode":
-        decrypt(text, shift)
+def crypt(input_text, shift_amount, choice):
+    if choice == "encode":
+        cipher_text = ""
+        for letter in input_text:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        print(f"The encoded text is {cipher_text}")
+    if choice == "decode":
+        plain_text = ""
+        for letter in input_text:
+            position = alphabet.index(letter)
+            new_position = position - shift_amount
+            new_letter = alphabet[new_position]
+            plain_text += new_letter
+        print(f"The decoded text is {plain_text}")
+
+crypt(input_text=text, shift_amount=shift, choice=direction)
+
+
