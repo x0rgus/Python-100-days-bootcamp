b510ac83d798d0865b45ebdf784333250e91f11d
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2023-07-25 13:20:48 -0300

    added calculator

---
+++ day 10/calculator.py
@@ -0,0 +1,15 @@
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+operation = {
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide,
+}
