780480c59932c1705a08c29d8ddea2464df3a346
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2023-07-27 13:52:13 -0300

    finished

--- day 10/calculator.py
+++ day 10/calculator.py
@@ -13,3 +13,12 @@ operation = {
     "*":multiply,
     "/":divide,
 }
+num1 = int(input("Please type the first number:"))
+num2 = int(input("please type the second number:"))
+for item in operation:
+    print(item)
+choice = input("Choose an operation:")
+
+calc_func = operation[choice]
+answer = calc_func(num1, num2)
+print(answer)
