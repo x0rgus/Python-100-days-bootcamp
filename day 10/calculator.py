ca75490efcafb874a2774dc7148734be50c15de4
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2023-07-27 20:34:22 -0300

    while loop

--- day 10/calculator.py
+++ day 10/calculator.py
@@ -13,12 +13,27 @@ operation = {
     "*":multiply,
     "/":divide,
 }
-num1 = int(input("Please type the first number:"))
-num2 = int(input("please type the second number:"))
-for item in operation:
-    print(item)
-choice = input("Choose an operation:")
+runflag = True
+while runflag == True:
+    num1 = int(input("Please type the first number:"))
+    num2 = int(input("please type the second number:"))
+    for item in operation:
+        print(item)
+    choice = input("Choose an operation:")
 
-calc_func = operation[choice]
-answer = calc_func(num1, num2)
-print(answer)
+    calc_func = operation[choice]
+    answer = calc_func(num1, num2)
+    print(f"{num1}{choice}{num2}={answer}")
+
+    choice = input("Pick another operation:")
+    calc_func = operation[choice]
+    num3 = int(input("pick another number:"))
+    answer = calc_func(answer, num3)
+    print(answer)
+    cont = input("do another calculation? y/n").lower()
+    if cont == "y":
+        runflag = True
+        continue
+    elif cont == "n":
+        runflag = False
+        break
