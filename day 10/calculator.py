2e777370c64722a2949e79ab7ed77721cbc5abd6
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2023-07-28 18:02:29 -0300

    added recursion

--- day 10/calculator.py
+++ day 10/calculator.py
@@ -13,27 +13,31 @@ operation = {
     "*":multiply,
     "/":divide,
 }
-runflag = True
-while runflag == True:
-    num1 = int(input("Please type the first number:"))
-    num2 = int(input("please type the second number:"))
-    for item in operation:
-        print(item)
-    choice = input("Choose an operation:")
+def calculation():
+    runflag = True
+    while runflag == True:
+        num1 = int(input("Please type the first number:"))
+        num2 = int(input("please type the second number:"))
+        for item in operation:
+            print(item)
+        choice = input("Choose an operation:")
 
-    calc_func = operation[choice]
-    answer = calc_func(num1, num2)
-    print(f"{num1}{choice}{num2}={answer}")
+        calc_func = operation[choice]
+        answer = calc_func(num1, num2)
+        print(f"{num1}{choice}{num2}={answer}")
 
-    choice = input("Pick another operation:")
-    calc_func = operation[choice]
-    num3 = int(input("pick another number:"))
-    answer = calc_func(answer, num3)
-    print(answer)
-    cont = input("do another calculation? y/n").lower()
-    if cont == "y":
-        runflag = True
-        continue
-    elif cont == "n":
-        runflag = False
-        break
+        choice = input("Pick another operation:")
+        calc_func = operation[choice]
+        num3 = int(input("pick another number:"))
+        answer = calc_func(answer, num3)
+        print(answer)
+        cont = input("do another calculation? y/n (type new for a new calculation)").lower()
+        if cont == "y":
+            runflag = True
+            continue
+        elif cont == "n":
+            runflag = False
+            break
+        elif cont == "new":
+            calculation()
+calculation()
