b93d9a2473ee4201b1598779ec13c21b80397a43
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2024-02-09 14:28:33 -0300

    added day 18

---
+++ Day 18/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+t1 = Turtle()
+t1.shape("classic")
+t1.color("red")
+
+#square
+for i in range(0,4):
+    t1.forward(100), t1.right(90)
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
