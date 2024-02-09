355d16d9857e06c77e0a5d6d6f098195899d3258
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2024-02-09 16:29:08 -0300

    added more shapes

--- Day 18/main.py
+++ Day 18/main.py
@@ -8,8 +8,25 @@ t1.color("red")
 for i in range(0,4):
     t1.forward(100), t1.right(90)
 
+t1.reset()
+#dotted lines
+for i in range(0,100):
+    t1.penup()
+    t1.forward(1)
+    if (i % 2) == 0:
+        t1.pendown()
+        t1.forward(1)
 
-
+t1.reset()
+# shape builder helper
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for x in range(num_sides):
+        t1.forward(100)
+        t1.right(angle)
+# if less than 3 sides its just a line so range starts from there
+for shape in range(3, 8):
+    print(shape)
 
 
 
