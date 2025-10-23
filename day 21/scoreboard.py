83748ebb71a19180f766a91bf570007aaf0aaf04
x0rgus <lucas.pr1@hotmail.com>
2025-10-23 17:12:26 -0300

    score tracking

--- day 21/scoreboard.py
+++ day 21/scoreboard.py
@@ -3,14 +3,18 @@ from turtle import Turtle
 class ScoreTurtle(Turtle):
     def __init__(self, xrange: int, yrange: int) -> None:
         super().__init__()
-        # self.hideturtle()
+        self.hideturtle()
+        
         self.color("white")
         self.speed("fastest")
         self.score = 0
-        self.ylocation = yrange / 2 - 20
+        self.xlocation = xrange / 4
+        self.ylocation = yrange / 4 
+
 
         self.teleport(xrange , self.ylocation)
     def addscore(self):
-        self.score =+ 1
+        self.score = self.score + 1
     def update(self):
-        self.write(arg=self.score)
+        self.clear()
+        self.write(f"Score:{self.score}", align="center", font=("Verdana", 24, "normal"))
