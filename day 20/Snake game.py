9f9d085eb2bd266f581decff41c6a64fdfd068ec
x0rgus <lucas.pr1@hotmail.com>
2025-10-23 13:19:42 -0300

    fix relative move

--- day 20/Snake game.py
+++ day 20/Snake game.py
@@ -5,7 +5,7 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("snake game")
 screen.tracer(0)
-REFRESHRATE = 0.4
+REFRESHRATE = 0.1
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -26,9 +26,9 @@ def up():
 def down():
     segments[0].seth(270)
 def left():
-    segments[0].seth(180)
+    segments[0].left(90)
 def right():
-    segments[0].seth(0)
+    segments[0].right(90)
 
 
 
@@ -42,6 +42,7 @@ while game_is_on:
         new_y = segments[seg_num - 1].ycor()
         segments[seg_num].goto(new_x, new_y)
     segments[0].forward(20)
+    left()
 
 
 
