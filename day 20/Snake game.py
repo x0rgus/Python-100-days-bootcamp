cd950ece620a66ad9fcee780287208c28cfcc8b6
x0rgus <lucas.pr1@hotmail.com>
2025-10-23 14:13:24 -0300

    turned snake into a class

--- day 20/Snake game.py
+++ day 20/Snake game.py
@@ -1,48 +1,32 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=1920, height=1080)
 screen.bgcolor("black")
 screen.title("snake game")
 screen.tracer(0)
-REFRESHRATE = 0.1
+REFRESHRATE = 0.4
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
 #game configs
-#generate starting snake
-for position in starting_positions:
-     new_segment = Turtle("square")
-     new_segment.penup()
-     new_segment.color("white")
-     new_segment.goto(position)
-     segments.append(new_segment)
 def tickrate():
     screen.update()
     time.sleep(REFRESHRATE)
-def up():
-    segments[0].seth(90)
-def down():
-    segments[0].seth(270)
-def left():
-    segments[0].left(90)
-def right():
-    segments[0].right(90)
 
 
+snake = Snake()
+snake.create_snake()
 
 game_is_on = True
-screen.update()
-while game_is_on:
-    tickrate()
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    left()
+while game_is_on:
+    
+    snake.move()
+    tickrate()
+    
+    
+
+
 
 
 
---
+++ day 20/snake.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+
+class Snake():
+    """A Snake"""
+    def __init__(self) -> None:
+        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        self.snake_alive = False
+        self.segments = []
+    def create_snake(self):
+        self.snake_alive = True
+        for position in self.starting_positions:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+        
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+            self.segments[0].forward(20)
+       
+    def turn(self, direction):
+        # turns the head
+        match direction:
+            case "left":
+                self.segments[0].left(90)
+            case "right":
+                self.segments[0].right(90)
+                
+    
+    
